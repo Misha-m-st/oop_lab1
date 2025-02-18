cba48535d9e28398bdc37afee6a7e6625aad4d77
Misha-m-st <mikhailmakarov2005@gmail.com>
2025-02-18 19:48:58 +0300

    First try

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "utils.h"
+
+int addition(int a, int b);
+int subtraction(int a, int b);
+
+int main() {
+    int a = 4;
+    int b = 9;
+    std::cout << "Sum: " << addition(a, b) << std::endl;
+    std::cout << "Subtraction: " << subtraction(a, b) << std::endl;
+    return 0;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,8 @@
+
+int addition(int a, int b) {
+    return a + b;
+}
+
+int subtraction(int a, int b) {
+    return a - b;
+}
